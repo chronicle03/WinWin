91ebe792e25d6b1e28cfd720511ab0a99feba6a3
Martiza Aurelia Ahmad <martizaaureliaahmad@if.uai.ac.id>
2023-06-19 13:53:05 +0700

    landing_page

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:winwin/pages/constant.dart";
+import "package:winwin/pages/landing_page2.dart";
 
 class LandingPage extends StatefulWidget {
   const LandingPage({Key? key}) : super(key: key);
@@ -10,120 +11,28 @@ class LandingPage extends StatefulWidget {
 
 class _LandingPageState extends State<LandingPage> {
   @override
-  Widget build(BuildContext context) {
-    Widget welcomingPage2Input() {
-      return Container(
-        padding: EdgeInsets.only(top: 129, left: 61, right: 62),
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: Image.asset(
-                '../assets/wp2.png',
-                height: 282,
-                width: 282,
-              ),
-            ),
-            const SizedBox(height: 86),
-            Column(
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Why WinWin?",
-                    style: textColor1TextStyle.copyWith(
-                      fontSize: 35,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Column(
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Improve your skills by trading",
-                    style: textColor1TextStyle.copyWith(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "skills for free.",
-                    style: textColor1TextStyle.copyWith(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 174),
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.asset(
-                      '../assets/lingkaran-1.png',
-                      width: 59,
-                      height: 15,
-                    ),
-                    Container(
-                      width: 177,
-                      height: 54,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: buttonColor,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          "Skip",
-                          style: textButtonTextStyle.copyWith(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LandingPage2()),
       );
-    }
+    });
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: ListView(
-        children: [
-          Container(
-            alignment: Alignment.center,
-            padding:
-                EdgeInsets.only(top: 259, bottom: 259, left: 57, right: 21),
-            child: Image.asset(
-              '../assets/Logo.png',
-              height: 282,
-              width: 282,
-            ),
-          ),
-          Column(
-            children: [
-              welcomingPage2Input(),
-            ],
-          ),
-        ],
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(top: 259, bottom: 259, left: 57, right: 21),
+        child: Image.asset(
+          'assets/Logo.png',
+          height: 282,
+          width: 282,
+        ),
       ),
     );
   }
---
+++ lib/pages/landing_page4.dart
@@ -0,0 +1,161 @@
+import "package:flutter/material.dart";
+import "package:winwin/pages/constant.dart";
+import "package:winwin/pages/login_page.dart";
+
+class LandingPage4 extends StatefulWidget {
+  const LandingPage4({Key? key}) : super(key: key);
+
+  @override
+  State<LandingPage4> createState() => _LandingPage4State();
+}
+
+class _LandingPage4State extends State<LandingPage4> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget welcomingPage4Input() {
+      return Container(
+        padding: EdgeInsets.only(top: 70, left: 61, right: 62),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/wp4.png',
+                height: 176,
+                width: 244,
+              ),
+            ),
+            const SizedBox(height: 60),
+            Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Join Us and Star",
+                    style: textColor1TextStyle.copyWith(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Your Journey!",
+                    style: textColor1TextStyle.copyWith(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 9),
+            Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Register now to become part of our",
+                    style: textColor1TextStyle.copyWith(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "community and start your journey",
+                    style: textColor1TextStyle.copyWith(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "towards building valuable relationships",
+                    style: textColor1TextStyle.copyWith(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "with fellow users.",
+                    style: textColor1TextStyle.copyWith(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 70),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      'assets/lingkaran-3.png',
+                      width: 59,
+                      height: 15,
+                    ),
+                    Container(
+                      width: 177,
+                      height: 54,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: buttonColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/login');
+                        },
+                        child: Text(
+                          "Skip",
+                          style: textButtonTextStyle.copyWith(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 44),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Column(
+        children: [
+          welcomingPage4Input(),
+        ],
+      ),
+    );
+  }
+}
