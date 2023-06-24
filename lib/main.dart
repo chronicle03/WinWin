5e7e05eae6259fc8a924a97e95e49c9e97a54815
Snuvbch <alvinfandi43@gmail.com>
2023-06-24 14:11:30 +0700

    ayee

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,21 @@
+import "package:flutter/material.dart";
+import "package:flutter_bloc/flutter_bloc.dart";
+import "package:winwin/bloc/user_bloc.dart";
+import "package:winwin/data/repository/user_repository.dart";
+import "package:winwin/pages/about.dart";
+import "package:winwin/pages/home/home_page.dart";
+import "package:winwin/pages/landing_page.dart";
+import "package:winwin/pages/login_page.dart";
+import "package:winwin/pages/validasi_login.dart";
+import "package:winwin/pages/verify_email_page.dart";
+import 'package:winwin/pages/about.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final UserRepositoryImpl userRepository = UserRepositoryImpl();
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +31,9 @@
         '/home': (context) => HomePage(),
         '/register': (context) => BlocProvider(
               create: (context) => UserBloc(userRepository),
-              child: RegisterPage(),
             ),
+        '/about': (context) => AboutPage(),
+        '/validasi-login': (context) => ValidasiLogin(),
       },
     );
   }
