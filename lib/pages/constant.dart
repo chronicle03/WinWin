1a1cefe2159527ec28d4c711c375b789c45bba8b
sayyiddrr <108316025+sayyiddrr@users.noreply.github.com>
2023-07-05 16:37:35 +0700

    merge

--- lib/pages/constant.dart
+++ lib/pages/constant.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-String baseUrl = "http://192.168.100.242:8000/api";
+//String baseUrl = "http://192.168.100.242:8000/api";
+String baseUrl = "http://localhost:8000/api";
 String baseUrlImage = "http://192.168.100.242:8000";
 
 Color backgroundColor = Color(0xff30444F);
